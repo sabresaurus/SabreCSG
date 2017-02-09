5a6aa5d6fd4db23d099b2858d5075bb540e13232
Barnaby MacBook <barnaby@sabresaurus.com>
2017-02-09 16:38:58 +0000

    * Palette window now supports dragging in between entries * Double clicking an animator controller now opens the Animator window * Some work towards supporting double clicking folders to open them in the Project window - this currently works intermittently, needs further work to make it work more robustly

--- Scripts/UI/PaletteWindow.cs
+++ Scripts/UI/PaletteWindow.cs
@@ -4,11 +4,13 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using System.Collections.Generic;
 using System.Text;
+using System.Reflection;
 
 namespace Sabresaurus.SabreCSG
 {
     public class PaletteWindow : EditorWindow
     {
+		enum DropInType { Replace, InsertAfter };
         const int SPACING = 8;
 
 
@@ -19,11 +21,12 @@ namespace Sabresaurus.SabreCSG
 
         int mouseDownIndex = -1;
 
-        int dropInTarget = -1; // Current slot a DragDrop is being considered as a destination
+		int dropInTargetIndex = -1; // Current slot a DragDrop is being considered as a destination
+		DropInType dropInType = DropInType.Replace;
 
-        bool dragging = false; // If a DragDrop from a slot is in progress
+		bool isDragging = false; // If a DragDrop from a slot is in progress
 
-        int indexToRemove = -1;
+		int deferredIndexToRemove = -1;
 
 		protected virtual string PlayerPrefKey
 		{
@@ -63,12 +66,13 @@ namespace Sabresaurus.SabreCSG
         {
             Event e = Event.current;
             //Debug.Log(e.type);
-            Object newSelectedObject = null;
+            
 #if UNITY_5_4_OR_NEWER
 			int columnCount = (int)(Screen.width / EditorGUIUtility.pixelsPerPoint) / (width + SPACING);
 #else
             int columnCount = Screen.width / (width + SPACING);
 #endif
+
             scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
             GUILayout.Space(8);
             //GUILayout.Box(new GUIContent(), GUILayout.ExpandWidth(true));
@@ -79,13 +83,15 @@ namespace Sabresaurus.SabreCSG
                 selectedObjects.Add(null);
             }
 
-            if (e.rawType == EventType.MouseUp || e.rawType == EventType.MouseMove || e.rawType == EventType.DragPerform || e.rawType == EventType.DragUpdated)
+            if (e.rawType == EventType.MouseUp || e.rawType == EventType.MouseMove || e.rawType == EventType.DragUpdated)
             {
-                dropInTarget = -1;
+                dropInTargetIndex = -1;
+				dropInType = DropInType.Replace;
             }
 
-            
-            
+			Object deferredInsertObject = null;
+			int deferredInsertIndex = -1;
+
             for (int i = 0; i < selectedObjects.Count; i++)
             {
                 int columnIndex = i % columnCount;
@@ -97,13 +103,27 @@ namespace Sabresaurus.SabreCSG
                 }
                 
                 
-                newSelectedObject = DrawElement(e, selectedObjects[i], i);
+				Object newSelectedObject = null;
+				bool dropAccepted = false;
+				DrawElement(e, selectedObjects[i], i, out newSelectedObject, out dropAccepted);
 
-                if(newSelectedObject != selectedObjects[i])
-                {
-                    selectedObjects[i] = newSelectedObject;
-                    Save();
-                }
+				if(dropAccepted || newSelectedObject != selectedObjects[i])
+				{
+					if(dropInType == DropInType.InsertAfter)
+					{
+						// Defer the insert until after we've drawn the UI so we don't mismatch UI mid-draw
+						deferredInsertIndex = i + 1;
+						deferredInsertObject = newSelectedObject;
+					}
+					else if(newSelectedObject != selectedObjects[i])
+					{
+						selectedObjects[i] = newSelectedObject;
+						Save();
+					}
+				}
+                
+
+				GUILayout.Space(4);
 
                 if(columnIndex == columnCount-1 || i == selectedObjects.Count-1) // If last in row
                 {
@@ -112,9 +132,9 @@ namespace Sabresaurus.SabreCSG
                 }
             }
 
-			if (e.type == EventType.MouseDrag && !dragging && mouseDownIndex != -1 && selectedObjects[mouseDownIndex] != null)
+			if (e.type == EventType.MouseDrag && !isDragging && mouseDownIndex != -1 && selectedObjects[mouseDownIndex] != null)
             {
-                dragging = true;
+                isDragging = true;
                 DragAndDrop.PrepareStartDrag();
                 DragAndDrop.objectReferences = new Object[] { selectedObjects[mouseDownIndex] };
 //				DragAndDrop.paths = new string[] { selectedObjects[mouseDownIndex] };
@@ -123,7 +143,7 @@ namespace Sabresaurus.SabreCSG
 
             if (e.rawType == EventType.MouseUp || e.rawType == EventType.MouseMove || e.rawType == EventType.DragPerform || e.rawType == EventType.DragExited)
             {
-                dragging = false;
+                isDragging = false;
                 mouseDownIndex = -1;
             }
 
@@ -140,19 +160,35 @@ namespace Sabresaurus.SabreCSG
             lastRect.xMax -= 10;
             lastRect.xMin = lastRect.xMax - 60;
             
+			// User configurable tile size
             width = (int)GUI.HorizontalSlider(lastRect, width, 50, 100);
 
-            // Delete at the end of the OnGUI so we don't mismatch any groups
-            if(indexToRemove != -1)
+            // Delete at the end of the OnGUI so we don't mismatch any UI groups
+            if(deferredIndexToRemove != -1)
             {
-                selectedObjects.RemoveAt(indexToRemove);
-                indexToRemove = -1;
+                selectedObjects.RemoveAt(deferredIndexToRemove);
+                deferredIndexToRemove = -1;
                 Save();
             }
+
+			// Insert at the end of the OnGUI so we don't mismatch any UI groups
+			if(deferredInsertObject != null)
+			{
+				selectedObjects.Insert(deferredInsertIndex, deferredInsertObject);
+				Save();
+			}
+
+			// Carried out a DragPerform, so reset drop in states
+			if (e.rawType == EventType.DragPerform)
+			{
+				dropInTargetIndex = -1;
+				dropInType = DropInType.Replace;
+			}
         }
 
-        public Object DrawElement(Event e, Object selectedObject, int index)
+		public void DrawElement(Event e, Object selectedObject, int index, out Object newSelection, out bool dropAccepted)
         {
+			dropAccepted = false;
             EditorGUILayout.BeginVertical();
 
             Texture2D texture = null;
@@ -179,7 +215,7 @@ namespace Sabresaurus.SabreCSG
             GUIStyle style = new GUIStyle(GUI.skin.box);
             style.padding = new RectOffset(0, 0, 0, 0);
             style.alignment = TextAnchor.MiddleCenter;
-            //style.margin = new RectOffset(0, 0, 0, 0);
+            
             if(texture != null)
             {
                 GUILayout.Box(texture, style, GUILayout.Width(width), GUILayout.Height(width));
@@ -191,22 +227,30 @@ namespace Sabresaurus.SabreCSG
             Rect previewRect = GUILayoutUtility.GetLastRect();
             bool mouseInRect = previewRect.Contains(e.mousePosition);
 
-            //GUILayout.Label("Set Vertex Colors", SabreGUILayout.GetTitleStyle());
-            //GUIStyle style = EditorStyles.objectFieldThumb;
+			Rect insertAfterRect = new Rect(previewRect.xMax, previewRect.y, 8, previewRect.height);
+			bool mouseInInsertAfterRect = insertAfterRect.Contains(e.mousePosition);
+
 			selectedObject = EditorGUILayout.ObjectField(selectedObject, TypeFilter, false, GUILayout.Width(width));
 
-
-            if(dropInTarget == index)
+            if(dropInTargetIndex == index)
             {
-                DrawOutline(previewRect, 2, Color.blue);
+				if(dropInType == DropInType.InsertAfter)
+				{
+					DrawOutline(insertAfterRect, 2, Color.blue);
+				}
+				else
+				{
+	                DrawOutline(previewRect, 2, Color.blue);
+				}
             }
+
 
             if(mouseInRect && e.type == EventType.MouseDown)
             {
                 mouseDownIndex = index;
             }
 
-            if (mouseInRect && e.type == EventType.MouseUp && !dragging)
+            if (mouseInRect && e.type == EventType.MouseUp && !isDragging)
             {
                 if (e.button == 0)
                 {
@@ -216,7 +260,7 @@ namespace Sabresaurus.SabreCSG
                 {
                     if(selectedObject == null)
                     {
-                        indexToRemove = index;
+                        deferredIndexToRemove = index;
                     }
                     else
                     {
@@ -227,15 +271,30 @@ namespace Sabresaurus.SabreCSG
                 }
             }
 
-            if (mouseInRect && e.type == EventType.MouseDown && !dragging)
+            if (mouseInRect && e.type == EventType.MouseDown && !isDragging)
             {
                 if (e.button == 0)
                 {
-                    if (e.clickCount == 2 && selectedObject != null && selectedObject.GetType() == typeof(SceneAsset))
+                    if (e.clickCount == 2 && selectedObject != null)
                     {
-                        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        if (selectedObject.GetType() == typeof(SceneAsset))
                         {
-                            EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(selectedObject), OpenSceneMode.Single);
+                            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                            {
+                                EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(selectedObject), OpenSceneMode.Single);
+                            }
+                        }
+						else if (typeof(RuntimeAnimatorController).IsAssignableFrom(selectedObject.GetType()))
+						{
+							SetAnimatorWindow(selectedObject);
+						}
+                        else if(selectedObject.GetType() == typeof(DefaultAsset))
+                        {
+                            // Something without a strong Unity type, could be a folder
+                            if(ProjectWindowUtil.IsFolder(selectedObject.GetInstanceID()))
+                            {
+                                SetProjectWindowFolder(AssetDatabase.GetAssetPath(selectedObject));
+                            }
                         }
                     }
                 }
@@ -245,31 +304,86 @@ namespace Sabresaurus.SabreCSG
 
             if (e.type == EventType.DragUpdated || e.type == EventType.DragPerform)
             {
-				if (DragAndDrop.objectReferences.Length > 0
-					&& (DragAndDrop.objectReferences[0].GetType() == TypeFilter ||DragAndDrop.objectReferences[0].GetType().IsSubclassOf(TypeFilter)))
-				{
-	                if (mouseInRect)
-	                {
-	                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                if (DragAndDrop.objectReferences.Length > 0
+                    && (DragAndDrop.objectReferences[0].GetType() == TypeFilter || DragAndDrop.objectReferences[0].GetType().IsSubclassOf(TypeFilter)))
+                {
+					if (mouseInRect || mouseInInsertAfterRect)
+                    {
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
 
-	                    if (e.type == EventType.DragPerform)
-	                    {
-	                        DragAndDrop.AcceptDrag();
+                        if (e.type == EventType.DragPerform)
+                        {
+                            DragAndDrop.AcceptDrag();
 
                             selectedObject = DragAndDrop.objectReferences[0];
-	                    }
-	                    else
-	                    {
-	                        dropInTarget = index;
-	                    }
-	                    Repaint();
-	                }
-			}
+
+							dropAccepted = true;
+                        }
+                        else
+                        {
+                            dropInTargetIndex = index;
+							dropInType = mouseInInsertAfterRect ? DropInType.InsertAfter : DropInType.Replace;
+                        }
+                        Repaint();
+                    }
+                }
             }
 
             EditorGUILayout.EndVertical();
-            return selectedObject;
+
+			newSelection = selectedObject;
         }
+
+        public static void SetProjectWindowFolder(string folderPath)
+        {
+            // This code uses reflection to set the project window folder
+            // As a result the internal structure could change, so the entire thing is wrapped in a try-catch
+            try
+            {
+				EditorApplication.ExecuteMenuItem("Window/Project");
+
+                Assembly unityEditorAssembly = Assembly.GetAssembly(typeof(Editor));
+                if (unityEditorAssembly != null)
+                {
+                    System.Type projectWindowUtilType = unityEditorAssembly.GetType("UnityEditor.ProjectWindowUtil");
+                    MethodInfo getBrowserMethodInfo = projectWindowUtilType.GetMethod("GetProjectBrowserIfExists", BindingFlags.Static | BindingFlags.NonPublic);
+                    
+                    // Get any active project browser (Project window)
+                    object projectBrowser = getBrowserMethodInfo.Invoke(null, new object[] { });
+
+                    // If there is an active project browser
+                    if (projectBrowser != null)
+                    {
+                        System.Type projectBrowserType = unityEditorAssembly.GetType("UnityEditor.ProjectBrowser");
+
+                        System.Type searchFilterType = unityEditorAssembly.GetType("UnityEditor.SearchFilter");
+
+                        // Create a SearchFilter instance which we will populate then pass to the ProjectBrowser
+                        object searchFilterInstance = System.Activator.CreateInstance(searchFilterType);
+                        PropertyInfo foldersProperty = searchFilterType.GetProperty("folders");
+                        // Set the active folder on the new SearchFilter
+                        foldersProperty.GetSetMethod().Invoke(searchFilterInstance, new object[] { new string[] { folderPath } });
+
+                        // Supply the new SearchFilter to the project browser so it shows the new folder
+                        MethodInfo setSearchMethodInfo = projectBrowserType.GetMethod("SetSearch", BindingFlags.Instance | BindingFlags.Public, null, new System.Type[] { searchFilterType }, null);
+                        setSearchMethodInfo.Invoke(projectBrowser, new object[] { searchFilterInstance });
+
+                        // Folder selection has changed, make sure the tree and top bar properly update
+                        MethodInfo selectionChangedMethodInfo = projectBrowserType.GetMethod("FolderTreeSelectionChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+                        selectionChangedMethodInfo.Invoke(projectBrowser, new object[] { true });
+                    }
+                }
+            }
+            catch
+            {
+                // Failed, suppress any errors and just do nothing
+            }
+        }
+
+		public static void SetAnimatorWindow(Object selectedObject)
+		{
+			EditorApplication.ExecuteMenuItem("Window/Animator");
+		}
 
         void DrawOutline(Rect lastRect, int lineThickness, Color color)
         {
