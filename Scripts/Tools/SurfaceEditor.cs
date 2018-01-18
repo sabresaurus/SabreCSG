7c46cbbe3f46b310d826817d52e5d2fe64d4bb7d
Henry de Jongh <henry00@live.nl>
2018-01-18 09:38:42 +0100

    Clicking without any modifier keys cycles through faces and with modifier keys does a special action, that's either rotating on CTRL (which is actually behaving like this already) or quick select on SHIFT. Now we got every feature!

--- Scripts/Tools/SurfaceEditor.cs
+++ Scripts/Tools/SurfaceEditor.cs
@@ -180,10 +180,32 @@ namespace Sabresaurus.SabreCSG
                 List<Polygon> raycastHits = csgModel.RaycastBuiltPolygonsAll(ray).Select(hit => csgModel.GetSourcePolygon(hit.Polygon.UniqueIndex)).Where(item => item != null).ToList();
                 Polygon sourcePolygon = null;
 
-                // Use the first polygon that was hit so we don't accidentally select/deselect polygons behind the one we see.
-                if (raycastHits.Count > 0)
+                // User is trying to multiselect, let's not make life difficult for them by only accepting the nearest polygon
+                if (SabreInput.IsModifier(e, EventModifiers.Shift))
                 {
-                    sourcePolygon = raycastHits[0];
+                    // Use the first polygon that was hit
+                    if (raycastHits.Count > 0)
+                    {
+                        sourcePolygon = raycastHits[0];
+                    }
+                }
+                else
+                {
+                    // Walk through the hits from front to back and find if any of them are in the selection set
+                    for (int i = 0; i < raycastHits.Count; i++)
+                    {
+                        if (selectedSourcePolygons.Contains(raycastHits[i]))
+                        {
+                            sourcePolygon = raycastHits[i];
+                            break;
+                        }
+                    }
+
+                    // None of the hit polygons are in the selection set, so just use the first hit polygon if it's available
+                    if (sourcePolygon == null && raycastHits.Count >= 1)
+                    {
+                        sourcePolygon = raycastHits[0];
+                    }
                 }
 
                 // If a polygon has been hit
