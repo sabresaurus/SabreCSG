3cb40b47405a0d74da32e3a9e2d5e948ff664cb7
Henry de Jongh <henry00@live.nl>
2018-03-11 19:52:51 +0100

    Now hiding compound brushes from the "..." list that are alreay in the toolbar. Added small tutorial on how to add more compound brushes to the list. Added missing meta files.

--- Scripts/UI/Toolbar.cs
+++ Scripts/UI/Toolbar.cs
@@ -405,6 +405,10 @@ namespace Sabresaurus.SabreCSG
 					menu.AddItem (new GUIContent (compoundBrushTypes[i].Name), false, CreateCompoundBrush, compoundBrushTypes[i]);
 				}
 
+                menu.AddSeparator("");
+                
+                menu.AddItem(new GUIContent("Add More?"), false, () => { EditorUtility.DisplayDialog("SabreCSG - About Compound Brushes", "Any custom compound brushes in your project are automatically detected and added to this list. Simply inherit from 'Sabresaurus.SabreCSG.CompoundBrush'.", "Okay"); });
+
 				menu.DropDown(new Rect(60,createBrushStyle.fixedHeight, 100, createBrushStyle.fixedHeight));
 			}
 
