4aa8bea1ddeb8d68dc37b0dd25686194e1b8c98a
Henry de Jongh <henry00@live.nl>
2018-05-20 01:13:52 +0200

    Now excludes faces and removes special brushes that SabreCSG can't handle.

--- Scripts/Importers/ValveMapFormat2006/VmfWorldConverter.cs
+++ Scripts/Importers/ValveMapFormat2006/VmfWorldConverter.cs
@@ -32,6 +32,32 @@ namespace Sabresaurus.SabreCSG.Importers.ValveMapFormat2006
 #endif
                     VmfSolid solid = world.Solids[i];
 
+                    // don't add triggers to the scene.
+                    if (solid.Sides.Count > 0 && (
+                        solid.Sides[0].Material == "TOOLS/TOOLSTRIGGER" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSBLOCK_LOS" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSBLOCKBULLETS" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSBLOCKBULLETS2" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSBLOCKSBULLETSFORCEFIELD" || // did the wiki have a typo or is BLOCKS truly plural?
+                        solid.Sides[0].Material == "TOOLS/TOOLSBLOCKLIGHT" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSCLIMBVERSUS" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSHINT" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSINVISIBLE" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSINVISIBLENONSOLID" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSINVISIBLELADDER" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSINVISMETAL" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSNODRAWROOF" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSNODRAWWOOD" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSNODRAWPORTALABLE" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSSKIP" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSFOG" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSSKYBOX" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLS2DSKYBOX" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSSKYFOG" ||
+                        solid.Sides[0].Material == "TOOLS/TOOLSFOGVOLUME"
+                        ))
+                        continue;
+
                     // build a very large cube brush.
                     var go = model.CreateBrush(PrimitiveBrushType.Cube, Vector3.zero);
                     var pr = go.GetComponent<PrimitiveBrush>();
@@ -41,7 +67,25 @@ namespace Sabresaurus.SabreCSG.Importers.ValveMapFormat2006
                     for (int j = solid.Sides.Count; j-- > 0;)
                     {
                         VmfSolidSide side = solid.Sides[j];
-                        ClipUtility.ApplyClipPlane(pr, new Plane(pr.transform.InverseTransformPoint(new Vector3(side.Plane.P1.X, side.Plane.P1.Z, side.Plane.P1.Y) / scale), pr.transform.InverseTransformPoint(new Vector3(side.Plane.P2.X, side.Plane.P2.Z, side.Plane.P2.Y) / scale), pr.transform.InverseTransformPoint(new Vector3(side.Plane.P3.X, side.Plane.P3.Z, side.Plane.P3.Y) / scale)), false);
+                        Plane clip = new Plane(pr.transform.InverseTransformPoint(new Vector3(side.Plane.P1.X, side.Plane.P1.Z, side.Plane.P1.Y) / scale), pr.transform.InverseTransformPoint(new Vector3(side.Plane.P2.X, side.Plane.P2.Z, side.Plane.P2.Y) / scale), pr.transform.InverseTransformPoint(new Vector3(side.Plane.P3.X, side.Plane.P3.Z, side.Plane.P3.Y) / scale));
+                        ClipUtility.ApplyClipPlane(pr, clip, false);
+
+                        // find the polygons associated with the clipping plane.
+                        // the normal is unique and can never occur twice as that wouldn't allow the solid to be convex.
+                        var polygons = pr.GetPolygons().Where(p => p.Plane.normal == clip.normal);
+                        foreach (var polygon in polygons)
+                        {
+                            // detect excluded polygons.
+                            if (side.Material == "TOOLS/TOOLSNODRAW")
+                                polygon.UserExcludeFromFinal = true;
+                            // detect collision-only brushes.
+                            if (side.Material == "TOOLS/TOOLSCLIP" ||
+                                side.Material == "TOOLS/TOOLSNPCCLIP" ||
+                                side.Material == "TOOLS/TOOLSPLAYERCLIP" ||
+                                side.Material == "TOOLS/TOOLSGRENDADECLIP" ||
+                                side.Material == "TOOLS/TOOLSSTAIRS")
+                                pr.IsVisible = false;
+                        }
                     }
                 }
 
