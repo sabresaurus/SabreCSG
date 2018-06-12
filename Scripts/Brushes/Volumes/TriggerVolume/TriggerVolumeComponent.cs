6b987a96ac628deaf7a9811324cea6c6740050b1
Daniel Cornelius <dcorn94@gmail.com>
2018-06-12 02:30:05 -0500

    Add SendMessage events
    + Add SendMessage() option to trigger volumes
    * TriggerVolumeComponent now accounts for both triggerMode and triggerEventType
    
    ! Known issues !
    - SendMessageEvent does not correctly serialize in the inspector, and does not update the value parameter like it should.
    - UnityEvent does not properly serialize on versions previous to Unity 2018. This is an internal unity bug.

--- Scripts/Brushes/Volumes/TriggerVolume/TriggerVolumeComponent.cs
+++ Scripts/Brushes/Volumes/TriggerVolume/TriggerVolumeComponent.cs
@@ -14,54 +14,108 @@ namespace Sabresaurus.SabreCSG
 		public TriggerVolumeEvent onEnterEvent;
 		public TriggerVolumeEvent onStayEvent;
 		public TriggerVolumeEvent onExitEvent;
+		public List<SendMessageEvent> smOnEnterEvent;
+		public List<SendMessageEvent> smOnStayEvent;
+		public List<SendMessageEvent> smOnExitEvent;
 
 		private bool isTriggered = false;
 
 		private void OnTriggerEnter( Collider other )
 		{
-			if( other.tag == filterTag && other.gameObject.layer == layerMask )
+			if( triggerMode == TriggerVolumeTriggerMode.Enter ||
+				triggerMode == TriggerVolumeTriggerMode.EnterExit ||
+				triggerMode == TriggerVolumeTriggerMode.EnterStay ||
+				triggerMode == TriggerVolumeTriggerMode.All )
 			{
-				if( isTriggered )
-					return;
-
-				if( triggerOnce )
+				if( other.tag == filterTag && other.gameObject.layer == layerMask )
 				{
-					isTriggered = true;
-				}
+					if( isTriggered )
+						return;
 
-				onEnterEvent.Invoke();
+					if( triggerOnce )
+					{
+						isTriggered = true;
+					}
+
+					if( volumeEventType == TriggerVolumeEventType.UnityEvent )
+					{
+						onEnterEvent.Invoke();
+					}
+					else
+					{
+						foreach( SendMessageEvent e in smOnEnterEvent )
+						{
+							e.target.SendMessage( e.message, e.value, SendMessageOptions.DontRequireReceiver );
+
+							//Debug.Log( "AAAAAAAAAAAAAAAAAAAA = Enter" );
+						}
+					}
+				}
 			}
 		}
 
 		private void OnTriggerExit( Collider other )
 		{
-			if( other.tag == filterTag && other.gameObject.layer == layerMask )
+			if( triggerMode == TriggerVolumeTriggerMode.Exit ||
+				triggerMode == TriggerVolumeTriggerMode.EnterExit ||
+				triggerMode == TriggerVolumeTriggerMode.All )
 			{
-				if( isTriggered )
-					return;
-
-				if( triggerOnce )
+				if( other.tag == filterTag && other.gameObject.layer == layerMask )
 				{
-					isTriggered = true;
-				}
+					if( isTriggered )
+						return;
 
-				onExitEvent.Invoke();
+					if( triggerOnce )
+					{
+						isTriggered = true;
+					}
+					if( volumeEventType == TriggerVolumeEventType.UnityEvent )
+					{
+						onExitEvent.Invoke();
+					}
+					else
+					{
+						foreach( SendMessageEvent e in smOnExitEvent )
+						{
+							e.target.SendMessage( e.message, e.value, SendMessageOptions.DontRequireReceiver );
+
+							//Debug.Log( "AAAAAAAAAAAAAAAAAAAA = Exit" );
+						}
+					}
+				}
 			}
 		}
 
 		private void OnTriggerStay( Collider other )
 		{
-			if( other.tag == filterTag && other.gameObject.layer == layerMask )
+			if( triggerMode == TriggerVolumeTriggerMode.Stay ||
+				triggerMode == TriggerVolumeTriggerMode.EnterStay ||
+				triggerMode == TriggerVolumeTriggerMode.All )
 			{
-				if( isTriggered )
-					return;
-
-				if( triggerOnce )
+				if( other.tag == filterTag && other.gameObject.layer == layerMask )
 				{
-					isTriggered = true;
-				}
+					if( isTriggered )
+						return;
 
-				onStayEvent.Invoke();
+					if( triggerOnce )
+					{
+						isTriggered = true;
+					}
+
+					if( volumeEventType == TriggerVolumeEventType.UnityEvent )
+					{
+						onStayEvent.Invoke();
+					}
+					else
+					{
+						foreach( SendMessageEvent e in smOnStayEvent )
+						{
+							e.target.SendMessage( e.message, e.value, SendMessageOptions.DontRequireReceiver );
+
+							//Debug.Log( "AAAAAAAAAAAAAAAAAAAA = Stay" );
+						}
+					}
+				}
 			}
 		}
 	}
---
+++ Scripts/Brushes/Volumes/TriggerVolume/TriggerVolumeParamTypeCode.cs
@@ -0,0 +1,11 @@
+﻿namespace Sabresaurus.SabreCSG
+{
+	public enum TriggerVolumeParamTypeCode : byte
+	{
+		None = 0,
+		String = 2,
+		Bool = 4,
+		Int = 8,
+		Float = 16
+	};
+}
