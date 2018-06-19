f0eec4dee7e22fe546e914fafc28218c6d6b342b
Henry de Jongh <henry00@live.nl>
2018-06-19 17:04:14 +0200

    Moved the Volume materials out of the Resources directory.

--- Scripts/Brushes/Volumes/TriggerVolume/TriggerVolume.cs
+++ Scripts/Brushes/Volumes/TriggerVolume/TriggerVolume.cs
@@ -70,7 +70,7 @@ namespace Sabresaurus.SabreCSG.Volumes
         {
             get
             {
-                return (Material)SabreCSGResources.LoadObject("Resources/Materials/scsg_volume_trigger.mat");
+                return (Material)SabreCSGResources.LoadObject("Materials/scsg_volume_trigger.mat");
             }
         }
 
